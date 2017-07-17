0ca837e3c8dd0ccf90ec1348e639501c6a78c5f8
wangyiming <wymm0008@gmail.com>
2017-07-17 16:29:14 +0800

    cdkey router

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import * as Router from 'koa-router'
 import url from './constants/url'
 import account from './controllers/account';
+import cdkey from './controllers/cdkey'
 
 const router = new Router();
 
 router.post(url.ACCOUNT_CREATE, account.onCreate);
 router.post(url.ACCOUNT_LOGIN, account.onLogin);
+router.post(url.CDKEY_USE, cdkey.onUse);
 
 export default router
